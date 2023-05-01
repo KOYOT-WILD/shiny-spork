c5b0472ae7895a45c003864373ab062e89d27b00
KOYOT <vvvsonivvv@gmail.com>
2023-05-01 21:37:43 +0700

    Initial commit

---
+++ app/src/main/java/com/tomtom/online/sdk/displaymap/MainActivity.java
@@ -0,0 +1,13 @@
+package com.tomtom.online.sdk.displaymap;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+  }
+}
